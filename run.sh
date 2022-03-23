419b4f99324e4b3e5dde8f8eb7ec08fcd1d35049
Administrator <servicoinmetrics734472@userede.com.br>
2022-03-23 01:04:01 -0300

    update workflow

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 cd /home/ec2-user/fiap-hmv-service
 docker build --tag=visdocker10/hmv-service:latest --rm=true .
 docker push visdocker10/hmv-service
-docker run -p 8080:8080 visdocker10/hmv-service:latest
+docker stop hmv-service
+docker rm hmv-service
+docker run --name hmv-service -p 8080:8080 visdocker10/hmv-service:latest
